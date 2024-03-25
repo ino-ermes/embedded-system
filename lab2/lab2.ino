892bce58c865ec80ca2b6b56b091babf3a5768e9
ino_ermes <hikigayahachiman.akame.kill@gmail.com>
2024-03-25 21:51:57 +0700

    modify

--- lab2/lab2.ino
+++ lab2/lab2.ino
@@ -1,62 +1,40 @@
-int redPin = 9;
-int greenPin = 10;
-int bluePin = 11;
+const int ledPinRed = 9;
+const int ledPinGreen = 10;
+const int ledPinBlue = 11;
 
 void setup() {
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
-
   Serial.begin(9600);
+  pinMode(ledPinRed, OUTPUT);
+  pinMode(ledPinGreen, OUTPUT);
+  pinMode(ledPinBlue, OUTPUT);
 }
-
-int value = 0;
 
 void loop() {
-  if (Serial.available()) {
-    String input = Serial.readString();
+  if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    int rIndex = data.indexOf('R');
+    int gIndex = data.indexOf('G');
+    int bIndex = data.indexOf('B');
+    int tIndex = data.indexOf('D');
 
-    int redIdx = input.indexOf('R');
-    int greenIdx = input.indexOf('G');
-    int blueIdx = input.indexOf('B');
-    int delayIdx = input.indexOf('D');
+    int redValue = data.substring(rIndex + 1, gIndex).toInt();
+    int greenValue = data.substring(gIndex + 1, bIndex).toInt();
+    int blueValue = data.substring(bIndex + 1).toInt();
+    int timeValue = data.substring(tIndex + 1).toInt();
 
-    if (redIdx != -1) {
-      value = myToInt(input, redIdx + 1);
-      analogWrite(redPin, constrain(value, 0, 255));
-    }
-    if (greenIdx != -1) {
-      value = myToInt(input, greenIdx + 1);
-      analogWrite(greenPin, constrain(value, 0, 255));
-    }
-    if (blueIdx != -1) {
-      value = myToInt(input, blueIdx + 1);
-      analogWrite(bluePin, constrain(value, 0, 255));
-    }
-    if (delayIdx != -1) {
-      value = myToInt(input, delayIdx + 1);
-      delay(constrain(value, 0, 999) * 1000);
-      analogWrite(redPin, 0);
-      analogWrite(greenPin, 0);
-      analogWrite(bluePin, 0);
+    redValue = min(redValue, 255);
+    greenValue = min(greenValue, 255);
+    blueValue = min(blueValue, 255);
+
+    if (rIndex != -1) analogWrite(ledPinRed, redValue);
+    if (gIndex != -1) analogWrite(ledPinGreen, greenValue);
+    if (bIndex != -1) analogWrite(ledPinBlue, blueValue);
+
+    if (tIndex != -1) {
+      delay(timeValue * 1000);
+      analogWrite(ledPinRed, 0);
+      analogWrite(ledPinGreen, 0);
+      analogWrite(ledPinBlue, 0);
     }
   }
-}
-
-int myToInt(const String &str, const int &startIndex) {
-  int len = str.length();
-
-  bool valid = 0 <= startIndex && startIndex < len;
-  if (!valid) return 0;
-
-  int endIndex = startIndex - 1;
-  while ((endIndex + 1) < len && '0' <= str.charAt(endIndex + 1) && str.charAt(endIndex + 1) <= '9')
-    ++endIndex;
-  int result = 0;
-  int scale = 1;
-  for (int i = endIndex; i >= startIndex; --i) {
-    result += scale * (str.charAt(i) - '0');
-    scale *= 10;
-  }
-  return result;
 }
